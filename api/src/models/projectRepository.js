34c5fc8f10432939c2a3cce749bbf55b523c6814
carlostr <croman@ibl.mx>
2025-09-26 08:44:09 -0600

    feat: agregar CRUD completo de projects, tambien corregir bug en el que faltan placeholders en createProject

--- api/src/models/projectRepository.js
+++ api/src/models/projectRepository.js
@@ -12,10 +12,46 @@ class ProjectRepository {
 
     async createProject(project, userId) {
         const [result] = await this.pool.query(`
-                INSERT INTO projects (name, description, status, start_date, end_date, user_id)`, 
-                [project.name, project.description, project.status, project.startDate, projectEndDate, userId])
+            INSERT INTO projects (name, description, status, start_date, end_date, user_id)
+            VALUES (?, ?, ?, ?, ?, ?)
+        `, [project.name, project.description, project.status, project.startDate, project.endDate, userId]);
         return querySuccess(result.affectedRows);
     }
+
+    async updateProject(project) {
+        const [result] = await this.pool.query(`
+            UPDATE projects SET name = ?, description = ?, status = ?, start_date = ?,
+                end_date = ?, user_id = ? WHERE id = ?
+            `, [project.name, project.description, project.status, project.startDate, 
+                project.endDate, project.userId, project.id
+            ]);
+        return querySuccess(result.affectedRows);
+    }
+
+    async getAllProjects() {
+        const [rows] = await this.pool.query(`
+            SELECT * FROM projects
+            `)
+        return rows;
+    }
+
+    async getProjectById(id) {
+        const [rows] = await this.pool.query(`
+            SELECT * FROM projects WHERE id = ?
+            `, [id]);
+        if (rows.length === 0) {
+            return null;
+        }
+        return rows[0];
+    }
+
+    async deleteProjectById(id) {
+        const [result] = await this.pool.query(`
+            DELETE FROM projects WHERE id = ?
+            `, [id]);
+        return querySuccess(result.affectedRows);
+    }
+
 }
 
 module.exports = ProjectRepository;
